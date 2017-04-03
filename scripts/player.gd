56503f5aabe6382f4454ca0d5ed3b58710bb5725
Pedro Zuppelli <pedro.zuppelli@hotmail.com>
2017-04-03 09:31:18 -0300

    Adds player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,16 @@
 extends RigidBody2D
 
 # class member variables go here, for example:
-# var a = 2
+var speed = 100
 # var b = "textvar"
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	print(get_name())
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		set_linear_velocity(Vector2(speed, get_linear_velocity().y))
+	elif Input.is_action_pressed("ui_left"):
+		set_linear_velocity(Vector2(-speed, get_linear_velocity().y))
